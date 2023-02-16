360ac7d893866ca66cdd1289e857f5f49e7a6c4d
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-16 14:57:17 +0000

    create Dataset class

---
+++ Assignment1/src/test/Dataset.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Hashtable;
+
+import task.Task;
+
+public class Dataset {
+
+	private Hashtable<Integer, Float> distribution;
+	public void setDistribution(Hashtable<Integer, Float> distribution) {
+		// TODO Auto-generated method stub
+		this.distribution = distribution;
+	}
+	public Task[] getData(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
