1cc09f64fb022a68a4024e69d181e37555076925
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-16 10:33:16 +0000

    create priority queue

---
+++ Assignment1/src/priorityQueue/PriorityQueueByArr.java
@@ -0,0 +1,37 @@
+package priorityQueue;
+import queue.Queue;
+import task.Task;
+
+public class PriorityQueue implements Queue{
+
+	@Override
+	public void enqueue(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Task dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
