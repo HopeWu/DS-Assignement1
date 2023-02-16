468fe26618c0a5fd5df9ad9def526d979806aea3
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-16 14:56:57 +0000

    create cpu

---
+++ Assignment1/src/test/Cpu.java
@@ -0,0 +1,20 @@
+package test;
+
+import queue.Queue;
+import task.Task;
+
+public class Cpu {
+	private Queue queue;
+	
+	Cpu(Queue queue){
+		this.queue = queue;
+	}
+	
+	public void perform(Task[] tasks) {
+		
+	}
+	
+	public void perform(Task task) {
+		
+	}
+}
