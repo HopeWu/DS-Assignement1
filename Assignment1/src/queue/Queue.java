a529e203c819418e1d1a96d7b9d856aaac9abb23
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-16 15:43:10 +0000

    update the queue interface

--- Assignment1/src/queue/Queue.java
+++ Assignment1/src/queue/Queue.java
@@ -6,6 +6,8 @@ public interface Queue {
 	public void enqueue(Task task);
 	public Task dequeue();
 	public Task peek();
+	// empty this queue
+	public void empty();
 	public boolean isEmpty();
 	public int size();
 } 
