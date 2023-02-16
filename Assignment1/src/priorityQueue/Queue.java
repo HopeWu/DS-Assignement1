176badcf7b036274f7880d12753a75f86a77d9ae
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-16 09:54:48 +0000

    create queue interface

---
+++ Assignment1/src/priorityQueue/Queue.java
@@ -0,0 +1,11 @@
+package priorityQueue;
+import task.Task;
+
+public interface Queue {
+	
+	public void enqueue(Task task);
+	public Task dequeue();
+	public Task peek();
+	public boolean isEmpty();
+	public int size();
+} 
