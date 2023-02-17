f22c1dd83709b07c9b0b15ad91fa31b0ed6d4094
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-17 20:56:00 +0000

    create another implementation

---
+++ Assignment1/src/standardQueue/StandardQueueByLinkedList.java
@@ -0,0 +1,50 @@
+package standardQueue;
+
+import queue.Queue;
+import task.Task;
+
+public class StandardQueueByLinkedList implements Queue {
+
+	@Override
+	public void enqueue(Task task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Task dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void empty() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
