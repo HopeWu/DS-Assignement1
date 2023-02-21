0c6b29cd456f7a9cbfac291f9fc61b6e4869d3d3
prateekdash <prateekdash>
2023-02-21 13:15:24 +0000

    Implemented Half Priority Queue using Singly Linked List and added comments in HalfPrioQueueByArr.java

--- Assignment1/src/halfPrioQueue/HalfPrioQueueByArr.java
+++ Assignment1/src/halfPrioQueue/HalfPrioQueueByArr.java
@@ -3,6 +3,16 @@ package halfPrioQueue;
 import queue.Queue;
 import task.Task;
 
+/**
+ * Half Priority Queue is functionally similar to a Priority 
+ * Queue, except that it dequeues and peeks the element with 
+ * highest priority for the last 'n' elements of the queue, 
+ * where n is "ideally" half the size of the queue. This 
+ * implementation uses Arrays.
+ * 
+ * @author Prateek Dash
+ *
+ */
 public class HalfPrioQueueByArr implements Queue {
 
 	private Task[] queue;    
@@ -13,13 +23,21 @@ public class HalfPrioQueueByArr implements Queue {
 		queue = new Task[length];
 		size = 0;
 	}
-
+	
+	/**
+	 * Adds an element to the rear of the queue.
+	 * @param task: the element that is to be added to the queue
+	 */
 	@Override
 	public void enqueue(Task task) {
 		if(isFull()) throw new RuntimeException("This queue is full.");		
 		queue[size++] = task;
 	}
 
+	/**
+	 * Removes an element with the highest priority within the specified
+	 * THRESHOLD value.
+	 */
 	@Override
 	public Task dequeue() {
 		if(isEmpty()) throw new RuntimeException("The queue is empty.");
@@ -37,33 +55,56 @@ public class HalfPrioQueueByArr implements Queue {
 		return task;
 	}
 
+	/**
+	 * Gets the element with the highest priority within the specified
+	 * THRESHOLD value.
+	 */
 	@Override
 	public Task peek() {
 		if(isEmpty()) throw new RuntimeException("The queue is empty.");	
 		return queue[getIndexOfMaxPriorityElement()];
 	}
 
+	/**
+	 * Checks whether the queue is empty.
+	 */
 	@Override
 	public boolean isEmpty() {		
 		return size == 0;
 	}
 
+	/**
+	 * Gets the number of the elements in the queue.
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * Clears the queue.
+	 */
 	@Override
 	public void empty() {
 		size = 0;
         queue = new Task[queue.length];
 	}
 
+	/**
+	 * Checks whether the queue is full.
+	 */
 	@Override
 	public boolean isFull() {
 		return size == queue.length;
 	}
 	
+	/**
+	 * Retrieves the index of task with highest importance value within 
+	 * the last 'n' elements. The 'n' value is set by THRESHOLD constant.
+	 * E.g. If THRESHOLD value is 5 and the size of queue is 10, then  
+	 * it would iterate through the elements 5 to 10 and returns the 
+	 * index of task with max importance value.
+	 */
 	private int getIndexOfMaxPriorityElement() {		
 		int index =  size - 1;
 		int lastIndex = size < THRESHOLD ? 0 : size - THRESHOLD;
--- Assignment1/src/halfPrioQueue/HalfPrioQueueByLinkedList.java
+++ Assignment1/src/halfPrioQueue/HalfPrioQueueByLinkedList.java
@@ -1,50 +1,107 @@
 package halfPrioQueue;
 
+import linkedList.SingleLink;
 import queue.Queue;
 import task.Task;
 
+/**
+ * Half Priority Queue is functionally similar to a Priority 
+ * Queue, except that it dequeues and peeks the element with 
+ * highest priority for the last 'n' elements of the queue, 
+ * where n is "ideally" half the size of the queue. This 
+ * implementation uses Linked List.
+ * 
+ * @author Prateek Dash
+ *
+ */
+
 public class HalfPrioQueueByLinkedList implements Queue {
 
+	private SingleLink<Task> queue = new SingleLink<>();
+	private final int THRESHOLD = 5;
+	
+	/**
+	 * Adds an element to the rear of the queue.
+	 * @param task: the element that is to be added to the queue
+	 */
 	@Override
 	public void enqueue(Task task) {
-		// TODO Auto-generated method stub
-		
+		queue.insertFromTail(task);
 	}
-
+	
+	/**
+	 * Removes an element with the highest priority within the specified
+	 * THRESHOLD value.
+	 */
 	@Override
 	public Task dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) throw new RuntimeException("The queue is empty.");
+		
+		int index = getIndexOfMaxPriorityElement();
+		Task task = queue.getNode(index).getData();
+		queue.remove(index);
+		return task;
 	}
 
+	/**
+	 * Gets the element with the highest priority within the specified
+	 * THRESHOLD value.
+	 */
 	@Override
 	public Task peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty()) throw new RuntimeException("The queue is empty.");
+		return queue.getNode(getIndexOfMaxPriorityElement()).getData();
 	}
 
+	/**
+	 * Checks whether the queue is empty.
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size() == 0;
 	}
-
+	
+	/**
+	 * Gets the number of the elements in the queue.
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return queue.getLength();
 	}
 
+	/**
+	 * Clears the queue.
+	 */
 	@Override
 	public void empty() {
-		// TODO Auto-generated method stub
-		
+		queue.clear();
 	}
 
+	/**
+	 * Checks whether the queue is full.
+	 */
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	/**
+	 * Retrieves the index of task with highest importance value within 
+	 * the last 'n' elements. The 'n' value is set by THRESHOLD constant.
+	 * E.g. If THRESHOLD value is 5 and the size of queue is 10, then  
+	 * it would iterate through the elements 5 to 10 and returns the 
+	 * index of task with max importance value.
+	 */
+	private int getIndexOfMaxPriorityElement() {
+		int index = size();
+		int lastIndex = size() < THRESHOLD ? 0 : size() - THRESHOLD;
 
+		for(int i = index; i > lastIndex; i--) {
+			if(queue.getNode(i).getData().getImportance() > queue.getNode(index).getData().getImportance()) {
+				index = i;
+			}
+		}
+		
+		return index;
+	}
 }
