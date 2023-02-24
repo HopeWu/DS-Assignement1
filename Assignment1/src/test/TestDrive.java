696ab32136ffc850a89685c8d75ff4a52f564b5b
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-24 21:58:47 +0000

    refactor test drive

--- Assignment1/src/test/TestDrive.java
+++ Assignment1/src/test/TestDrive.java
@@ -15,15 +15,22 @@ import task.Task;
 public class TestDrive {
 	static public void main(String[] string) {
 
+		experimentOne();
+	}
+	
+	static void experimentOne() {
+		
 		EfficiencyTest efficiencyTest = new EfficiencyTest();
 		
 		efficiencyTest.setQueue1(new StandardQueueByLinkedList());
-//		efficiencyTest.setQueue2(new PriorityQueueByDoublyLinkedList());
 		efficiencyTest.setQueue2(new HalfPrioQueueByLinkedList());
 		efficiencyTest.setDatasize(1000);
 		efficiencyTest.setBatchSize(100);
 		efficiencyTest.setDatasetProbability(100, 0.1);
 		efficiencyTest.setDatasetProbability(1, 1.0);
 		efficiencyTest.run();
+	}
+	static void experimentTwo() {
+		
 	}
 }
