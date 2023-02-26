161cf9f06dc0c3ca385d0cb1ed7243ff336902fb
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-26 13:23:50 +0000

    rebase main on dev-haopeng

--- Assignment1/src/priorityQueue/PriorityQueueByDoublyLinkedList.java
+++ Assignment1/src/priorityQueue/PriorityQueueByDoublyLinkedList.java
@@ -4,12 +4,8 @@ import linkedList.DoubleLink;
 import queue.Queue;
 import task.Task;
 
-<<<<<<< HEAD
-public class PriorityQueueByDoublyLinkedList implements Queue{
-=======
-public class PriorityQueueByDoublyLinkedList extends Queue{
->>>>>>> 514f387 (let every implementation extends instead of implementing Queue)
-	
+
+public class PriorityQueueByDoublyLinkedList extends Queue{	
 	private DoubleLink<Task> queue = new DoubleLink<Task>();
 	
 	/**
