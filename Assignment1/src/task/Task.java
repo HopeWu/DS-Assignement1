3508321e1393d0a6706942661991dcd0cab35d9a
HopeWu <30311646+HopeWu@users.noreply.github.com>
2023-02-26 12:20:02 +0000

    update comments

--- Assignment1/src/task/Task.java
+++ Assignment1/src/task/Task.java
@@ -18,7 +18,6 @@ public class Task {
 		try {
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return getImportance();
